afcd6b522d2e60aac61181060e425e15b44ee902
TriNamDo <444979@student.fontys.nl>
2019-12-01 14:16:58 +0100

    Username made unique & create profile template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Dashboard</h1>
-
-    <div class="py-4">
-        @if (session('status'))
-            <div class="">
-                {{ session('status') }}
-            </div>
-        @endif
-
-        You are logged in!
+<!--Header of personal profile page-->
+<div class="row">
+    <div class="col-3 p-5">
+        <img src="https://scontent-ams4-1.cdninstagram.com/vp/997a569d88c3924d1c92313ccdb0781d/5E88BFD8/t51.2885-19/s320x320/76880688_2770079693023939_7526676684385288192_n.jpg?_nc_ht=scontent-ams4-1.cdninstagram.com" class="rounded-circle" height=190px>
     </div>
+    <div class="col-9 p-5">
+        <div><h2>Username</h2></div>
+        <div class="d-flex">
+            <div class="pr-5"><strong>32</strong> posts</div>
+            <div class="pr-5"><strong>224</strong> followers</div>
+            <div class="pr-5"><strong>447</strong> following</div>
+        </div>
+        <div>
+            <!--description goes here-->
+        </div>
+    </div>
+</div>
+<!--Posts go here-->
+<div class="row">
+    
+</div>
 @endsection
