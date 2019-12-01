8ed3013cf834e4567322c733de9ede5ee98c6558
TriNamDo <444979@student.fontys.nl>
2019-12-01 23:13:53 +0100

    Prevent user not found error

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,7 +9,7 @@ class ProfileController extends Controller
 {
     public function index($user)
     {
-        $user = User::find($user);
+        $user = User::findOrFail($user);
         //TODO: if the numbers are bigger than 10k, divide them by 1k before passing to the view
         return view('home',[
             'user' => $user,
