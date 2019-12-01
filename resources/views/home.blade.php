0adef3b6175ed216028e5792334ffdb5d5d6e8a5
TriNamDo <444979@student.fontys.nl>
2019-12-01 16:59:22 +0100

    Created Profile model

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
         <img src="https://scontent-ams4-1.cdninstagram.com/vp/997a569d88c3924d1c92313ccdb0781d/5E88BFD8/t51.2885-19/s320x320/76880688_2770079693023939_7526676684385288192_n.jpg?_nc_ht=scontent-ams4-1.cdninstagram.com" class="rounded-circle" height=190px>
     </div>
     <div class="col-9 p-5">
-        <div><h2>Username</h2></div>
+        <div><h2>{{$user->name}}</h2></div>
         <div class="d-flex">
             <div class="pr-5"><strong>32</strong> posts</div>
             <div class="pr-5"><strong>224</strong> followers</div>
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Route::get('/test', 'TestController@test');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/profile/{user}', 'ProfileController@index')->name('profile.show');
