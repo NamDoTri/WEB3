d1b13ae3c6ce48358709fa0f34949ccdf17258ac
Delcroix,Nina N <n.delcroix@student.fontys.nl>
2019-12-10 18:35:38 +0100

    Nina

--- resources/views/picture/edit.blade.php
+++ resources/views/picture/edit.blade.php
@@ -1,8 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<h2 style="margin-top: 12px;" class="text-center">Edit Product</a></h2>
-<br>
+<h2 class="text-center m-4">Edit Image</h2>
+
+@if ($errors->any())
+    <div class='mb-4'>
+        <ul class='list-group'>
+            @foreach ($errors->all() as $error)
+                <li class='list-group-item list-group-item-danger'>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
  
 <form 
     action="{{ route('pictures.update', $picture_info->id) }}" 
@@ -13,15 +22,22 @@
 {{ csrf_field() }}
 @method('PATCH')
  
-<div class="row">
-<div class="form-group">
-        <label for="file">Update file:</label>
-        <input type="file" class="form-control" name="file"/>
-    </div>
+<div class="input-group m-4">
+  <div class="custom-file mr-4">
+    <input type="file" class="custom-file-input" name='file'>
+    <label class="custom-file-label" for="file">Choose file</label>
+  </div>
+</div>
+
     <div class="col-md-12">
         <button type="submit" class="btn btn-primary">Upload image</button>
     </div>
-</div>
  
 </form>
+<script type="application/javascript">
+    $('input[type="file"]').change((e) => {
+        const fileName = e.target.files[0].name;
+        $('.custom-file-label').html(fileName);
+    });
+</script>
 @endsection
