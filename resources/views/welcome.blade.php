7e94004c2379decf1573dd1fbf32441684d0ea7f
unknown <i444979@fhict.nl>
2020-01-12 13:01:42 +0100

    Added "show critiques" links to welcom view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,17 +3,18 @@
 @section('content')
 <h2 class="text-center m-4">Recent images</a></h2>
     <div class="row">
-              @foreach($pictures as $picture)
-              <div class='col-12 m-3 align-self-center'>
-                <div class="card">
+        @foreach($pictures as $picture)
+        <div class='col-12 m-3 align-self-center'>
+            <div class="card">
                 <img src="{{ $picture->filepath }}" class="card-img-top" alt="picture"> <!-- style="height: 30rem; width: auto; object-fit: cover;" -->
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $picture->caption }}</h5>
-                        <p class="card-text">Posted by <a href='/profile/{{ $picture->user_id }}'>{{ $picture->name }}</a></p>
-                        <p class="card-text text-right"><small class="text-muted">{{ date('Y-m-d', strtotime($picture->created_at)) }}</small></p>
-                    </div>
+                <div class="card-body">
+                    <h5 class="card-title">{{ $picture->caption }}</h5>
+                    <p class="card-text">Posted by <a href='/profile/{{ $picture->user_id }}'>{{ $picture->name }}</a></p>
+                    <p class="card-text text-right"><small class="text-muted">{{ date('Y-m-d', strtotime($picture->created_at)) }}</small></p>
+                    <a href="{{route('pictures.show', $picture->id )}}">Show critiques</a>
                 </div>
-                </div>
-              @endforeach
+            </div>
+        </div>
+        @endforeach
    </div>
 @endsection
